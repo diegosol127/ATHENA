f004a8fc624245a10d58ad45058d0c66b7efe522
diegosol127 <diegosol127>
2023-04-27 03:55:22 -0500

    correct camera image to get face vector

--- src/sensors.py
+++ src/sensors.py
@@ -29,6 +29,8 @@ class Sensor:
 		# load the pre-trained face detection model
 		self.file_dir = os.path.dirname(os.path.realpath(__file__)) + '/haarcascade_frontalface_default.xml'
 		self.face_cascade = cv2.CascadeClassifier(self.file_dir)
+
+		self. camera = picamera.PiCamera()
 		
 	def sense_distance(self):
 		time_start = time.time()
@@ -50,30 +52,29 @@ class Sensor:
 			self.prior_distance = distance
 		except:
 			distance = self.prior_distance
-			print('using prior distance')
+			# print('using prior distance')
 		return distance
 	
 	def sense_camera(self):
-		with picamera.PiCamera() as camera:
-			with picamera.array.PiRGBArray(camera) as stream:
+		with picamera.array.PiRGBArray(self.camera) as stream:
+			self.camera.resolution = (640,480)
+			self.camera.capture(stream, 'bgr', use_video_port=True)
+			frame = stream.array
+			frame_center = [self.camera.resolution[0]//2, self.camera.resolution[1]//2]
+			
+			gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-				camera.capture(stream, 'bgr', use_video_port=True)
-				frame = stream.array
-				frame_center = [frame.shape[1]//2, frame.shape[0]//2]
-				
-				gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+			faces = self.face_cascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=5)
+			face_vector_norm = []
 
-				faces = self.face_cascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=5)
-				face_vector_norm = []
+			if len(faces) > 0:
+				idx_max_face = np.argmax(faces[:,2]*faces[:,3])
+				(x, y, w, h) = faces[idx_max_face,:]
+				face_center = [x + w // 2, y + h // 2]
+				face_vector = [face_center[0] - frame_center[0], face_center[1] - frame_center[1]]
+				face_vector_norm = [face_vector[0]/max(frame.shape)*2,face_vector[1]/max(frame.shape)*2]
 
-				if len(faces) > 0:
-					idx_max_face = np.argmax(faces[:,2]*faces[:,3])
-					(x, y, w, h) = faces[idx_max_face,:]
-					face_center = [x + w // 2, y + h // 2]
-					face_vector = [face_center[0] - frame_center[0], face_center[1] - frame_center[1]]
-					face_vector_norm = [face_vector[0]/max(frame.shape)*2,face_vector[1]/max(frame.shape)*2]
-
-				return face_vector_norm
+			return face_vector_norm
 
 	def sense_audio(self):
 		pass
